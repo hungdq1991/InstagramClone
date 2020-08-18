89a2ccf0f68976edd5ff6371c92f24b2b87c6070
Quoc Hung <39042165+hungdq1991@users.noreply.github.com>
2020-08-18 16:29:05 +0700

    2020/08/18: Second commit

---
+++ app/src/main/java/com/example/instagram_clone/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.instagram_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.instagram_clone.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_home );
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById( R.id.bottomNavViewBar );
+        BottomNavigationViewHelper.setupBottomNavigationView( bottomNavigationViewEx );
+    }
+}
